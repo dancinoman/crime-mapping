48f273fecbee6d419ed6993e31ef967dca164b9e
Hugo Guay <dancinoman@gmail.com>
2025-07-09 11:43:06 -0400

    pipeline completed

--- app/convert_to_csv.py
+++ app/convert_to_csv.py
@@ -1,5 +1,6 @@
 import os
-import pandas
+import re
+import pandas as pd
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.dirname(script_dir)
@@ -17,21 +18,35 @@ def fix_values_shifted(df, row_number):
 
     return df[df.columns[:-1]]  # Remove the last column which is now empty
 
-for file_name in os.listdir(download_path_source):
-    pass
+def remove_quotes(df):
+    """Remove quotes from DataFrame column and values"""
+    df.columns = [col.replace('"', '') for col in df.columns.tolist()]
+    df.loc[:,"Catégorie"] = df["Catégorie"].map(lambda x: x.replace('"', '') if isinstance(x, str) else x)
+    return df
 
-# Extract duplicate columns
-with open(os.path.join(download_path_source, os.listdir(download_path_source)[0]), 'rb') as file:
-    df = pandas.read_excel(file, engine='openpyxl')
+def pivot(df):
+    df_melted = df.melt( id_vars='Catégorie', var_name='Région', value_name='Value')
+    df_pivoted = df_melted.pivot(index='Région', columns='Catégorie', values='Value').reset_index()
+    return df_pivoted
 
-    df_fixed = fix_values_shifted(df, 5)
+def execute_csv_creation():
+    """Execute the conversion of Excel files to CSV format."""
 
-    index_cols = df_fixed.columns[1:].tolist()
+    # Process each file in the source directory
+    for i,filename in enumerate(os.listdir(download_path_source)):
 
-    pivoted = df_fixed.pivot(
-        index=index_cols,
-        columns=df_fixed.columns[0],
-        values=df_fixed.columns[-1]
-    )
+        neighbour = re.search(r"_([^_.]+)\.[^.]+$", filename).group(1)
 
-    print(pivoted)
+        # Extract duplicate columns
+        with open(os.path.join(download_path_source, filename), 'rb') as file:
+            df = pd.read_excel(file, engine='openpyxl')
+
+            df_fixed = fix_values_shifted(df, 5)
+            df_fixed = remove_quotes(df_fixed)
+            df_pivoted = pivot(df_fixed)
+
+            # Process only the first file
+            if i == 0:
+                df_pivoted[:-2].to_csv(os.path.join(download_path_destination, "poverty_family_structure_city.csv"), index=False)
+
+            df_pivoted[-2:].to_csv(os.path.join(download_path_destination, f"poverty_family_structure_{neighbour}.csv"), index=False)
--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 from app import download_files as df
-
+from app import convert_to_csv as cc
+from app import export_csv as ec
 
 if __name__ == "__main__":
 
-    urls = df.get_urls()
-
-    for url in urls:
-
-        name = url.get("neighbourhood")
-        url = url.get("url")
-
-        print("Begin downloading file for neighbourhood:", name)
-        df.download_file_from_url(name, url)
-
-    
+    print("Starting the download process...")
+    df.download_file_from_url()
+    print("File downloaded now converting to CSV...")
+    cc.execute_csv_creation()
+    print("CSV files created, now uploading to S3...")
+    ec.upload_csv_to_s3()
+    print("Pipeline completed successfully!")
