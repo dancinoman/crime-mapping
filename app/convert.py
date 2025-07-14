a3e0a0251e8479ee572bb5be073fe5238d7abed9
Hugo Guay <dancinoman@gmail.com>
2025-07-14 17:47:31 -0400

    refactored and add json transformation

---
+++ app/convert.py
@@ -0,0 +1,96 @@
+import os
+import re
+import pandas as pd
+import json
+
+# Import custom modules
+from config import Path
+
+
+
+def convert_xlsx_to_csv():
+
+    path = Path("downloads", "xlsx-files", "csv-files")
+    path_source = path.get_source_path()
+    path_destination = path.get_destination_path()
+
+    def fix_values_shifted(df, row_number):
+        """Fix columns shifted by one row in the DataFrame."""
+        # Fix the first column shifted by one row
+        df.iloc[row_number,0] = str(df.iloc[row_number, 0]) + str(df.iloc[row_number, 1])
+
+        # Fix all other columns shifted by one row
+        for col in range(1, len(df.columns[1:])):
+            df.iloc[row_number, col] = df.iloc[row_number, col +1]
+
+        return df[df.columns[:-1]]  # Remove the last column which is now empty
+
+    def remove_quotes(df):
+        """Remove quotes from DataFrame column and values"""
+        df.columns = [col.replace('"', '') for col in df.columns.tolist()]
+        df.loc[:,"Catégorie"] = df["Catégorie"].map(lambda x: x.replace('"', '') if isinstance(x, str) else x)
+        return df
+
+    def pivot(df):
+        df_melted = df.melt( id_vars='Catégorie', var_name='Région', value_name='Value')
+        df_pivoted = df_melted.pivot(index='Région', columns='Catégorie', values='Value').reset_index()
+        return df_pivoted
+
+    def execute_poverty_creation():
+        """Execute the conversion of Excel files to CSV format."""
+
+        # Process each file in the source directory
+        for i,filename in enumerate(os.listdir(path_source)):
+
+            # Extract duplicate columns
+            with open(os.path.join(path_source, filename), 'rb') as file:
+                df = pd.read_excel(file, engine='openpyxl')
+
+                df_fixed = fix_values_shifted(df, 5)
+                df_fixed = remove_quotes(df_fixed)
+
+                #Filter the DataFrame with first file and filter the relevant columns for the others
+                if i == 0:
+                    df_filtered = df_fixed.iloc[:, :-2]
+                    neighbour = "zcity"
+                else:
+                    df_filtered = df_fixed.iloc[:, [0, -2, -1]]
+                    neighbour = re.search(r"_([^_.]+)\.[^.]+$", filename).group(1)
+
+                df_pivoted = pivot(df_filtered)
+                df_pivoted.to_csv(os.path.join(path_destination, f"poverty_family_structure_{neighbour}.csv"), index=False)
+
+    # Execute the conversion process
+    execute_poverty_creation()
+
+def convert_json():
+    """Convert Json to stackable data."""
+    path = Path("downloads", "json-files", "json-files")
+    path_source = path.get_source_path()
+    path_destination = path.get_destination_path()
+    file_name_source = "/municipality_polygon_map.json"
+    file_name_destination = "/polygon_map_translate.json"
+
+    with open(path_source + file_name_source, 'r') as file:
+        data = json.load(file)
+
+    stakable_data = []
+    arrond_to_skip = ["ROXBORO"]
+
+    for features in data['features']:
+
+        # skip too long arrondissements
+        if features['properties']['ARROND'] in arrond_to_skip:
+            break
+        stakable_data.append({
+            "id": features['properties']['OBJECTID'],
+            "name": features['properties']['ARROND'],
+            "type": features['type'],
+            "geometry": features['geometry']['type'],
+            "coordinates": features['geometry']['coordinates']
+        })
+
+    with open(path_destination + file_name_destination, 'w') as file:
+        for obj in stakable_data:
+            json.dump(obj, file)
+            file.write("\n")
--- app/download_files.py
+++ app/download_files.py
@@ -17,140 +17,151 @@ from botocore.exceptions import NoCredentialsError
 # ======== S3 settings ========
 s3 = boto3.client('s3')
 S3_BUCKET_NAME = 'crime-porverty-heatmap-data-analysis'
-S3_INPUT_JSON = 'url-links-poverty/urls-poverty.json'
-S3_OUTPUT_LOCAL_JSON = 'data/poverty/urls-poverty.json'
-S3_OUTPUT_BUCKET_KEY = 'data/poverty/xlsx-files'
-S3_FILE_LOCAL_TMP = "downloads/xlsx-files"
 
-def get_urls():
-    s3.download_file(S3_BUCKET_NAME, S3_INPUT_JSON, S3_OUTPUT_LOCAL_JSON)
-    print(f'File downloaded to {S3_OUTPUT_LOCAL_JSON}')
-
-    with open(S3_OUTPUT_LOCAL_JSON, 'r') as file:
-        urls = json.load(file)
-
-    return urls
 
 def download_file_from_url():
 
-    urls = get_urls()
+    S3_INPUT_JSON = 'url-links-poverty/urls-poverty.json'
+    S3_OUTPUT_LOCAL_JSON = 'rules/poverty/urls-poverty.json'
+    S3_OUTPUT_BUCKET_KEY = 'data/poverty/xlsx-files'
+    S3_FILE_LOCAL_TMP = "downloads/xlsx-files"
 
-    for url in urls:
-        name = url.get("neighbourhood")
-        url = url.get("url")
+    def get_urls():
+        s3.download_file(S3_BUCKET_NAME, S3_INPUT_JSON, S3_OUTPUT_LOCAL_JSON)
+        print(f'File downloaded to {S3_OUTPUT_LOCAL_JSON}')
 
-        print("Begin downloading file for neighbourhood:", name)
+        with open(S3_OUTPUT_LOCAL_JSON, 'r') as file:
+            urls = json.load(file)
+
+        return urls
+
+    def download_file():
+
+        urls = get_urls()
+
+        for url in urls:
+            name = url.get("neighbourhood")
+            url = url.get("url")
+
+            print("Begin downloading file for neighbourhood:", name)
 
 
-        # === Chrome Setup for Headless Downloads ===
-        # Fix the path to the script directory and project root
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        project_root = os.path.dirname(script_dir)
-        download_path = os.path.join(project_root, S3_FILE_LOCAL_TMP)
-        os.makedirs(download_path, exist_ok=True)
+            # === Chrome Setup for Headless Downloads ===
+            # Fix the path to the script directory and project root
+            script_dir = os.path.dirname(os.path.abspath(__file__))
+            project_root = os.path.dirname(script_dir)
+            download_path = os.path.join(project_root, S3_FILE_LOCAL_TMP)
+            os.makedirs(download_path, exist_ok=True)
 
-        # === Chrome Setup for Headless Downloads ===
-        options = Options()
-        options.add_argument("--headless=new")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        prefs = {
-            "download.default_directory": download_path,
-            "download.prompt_for_download": False,
-            "download.directory_upgrade": True,
-            "safeBrowse.enabled": True
-        }
+            # === Chrome Setup for Headless Downloads ===
+            options = Options()
+            options.add_argument("--headless=new")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            prefs = {
+                "download.default_directory": download_path,
+                "download.prompt_for_download": False,
+                "download.directory_upgrade": True,
+                "safeBrowse.enabled": True
+            }
 
-        options.add_experimental_option("prefs", prefs)
+            options.add_experimental_option("prefs", prefs)
 
-        # ======== Initialize WebDriver =============
-        driver = webdriver.Chrome(options=options)
+            # ======== Initialize WebDriver =============
+            driver = webdriver.Chrome(options=options)
 
-        # ======== Navigate to page ========
-        driver.get(url)
+            # ======== Navigate to page ========
+            driver.get(url)
 
-        # ======== Wait for document.readyState ========
-        WebDriverWait(driver, 20).until(lambda d: d.execute_script("return document.readyState") == "complete")
+            # ======== Wait for document.readyState ========
+            WebDriverWait(driver, 20).until(lambda d: d.execute_script("return document.readyState") == "complete")
 
-        # ======== Switch to iframe if present ========
-        try:
-            iframe = WebDriverWait(driver, 5).until(
-                EC.presence_of_element_located((By.TAG_NAME, "iframe"))
-            )
-            driver.switch_to.frame(iframe)
-        except Exception:
-            print("No iframe found or switch not needed.")
-
-        # Get initial list of files in the download directory
-        initial_files = set(os.listdir(download_path))
-
-        # Use download button
-        download_button = WebDriverWait(driver, 20).until(
-                EC.element_to_be_clickable(
-                    (By.XPATH, "//button[contains(@onclick, \"downloadXLSX('csv_generale_33'\")]")
-                )
-            )
-
-        driver.execute_script("arguments[0].scrollIntoView();", download_button)
-        download_button.click()
-
-        print("Download button clicked. Waiting for file to appear...")
-
-        # ======== Wait for the file to be downloaded ========
-        # This loop waits until a new file appears in the directory that is not a temporary Chrome file
-        # and its size stops changing, indicating the download is complete.
-        downloaded_file = None
-        timeout = 60  # seconds
-        start_time = time.time()
-
-        while time.time() - start_time < timeout:
-            current_files = set(os.listdir(download_path))
-            new_files = current_files - initial_files
-
-            for f in new_files:
-                file_path = os.path.join(download_path, f)
-                # Exclude Chrome's temporary download files and check for actual files
-                if not f.startswith(".com.google.Chrome.") and not f.endswith(".crdownload"):
-                    # Check if the file size is stable (download complete)
-                    initial_size = -1
-                    for _ in range(5):
-
-                        try:
-                            current_size = os.path.getsize(file_path)
-                        except FileNotFoundError:
-                            continue
-
-                        if initial_size == current_size and current_size > 0:
-                            downloaded_file = f
-                            break
-                        initial_size = current_size
-                        time.sleep(0.5)
-                    if downloaded_file:
-                        break
-            if downloaded_file:
-                break
-            time.sleep(1)
-
-        driver.quit()
-
-        if downloaded_file:
-            new_file_name = f"poverty_family_structure_{name}.xlsx"
-
-            local_file_path = os.path.join(download_path, downloaded_file)
-            renamed_file_path = os.path.join(download_path, new_file_name)
-            # Rename the downloaded file
-            os.rename(local_file_path, renamed_file_path)
-            s3_key = os.path.join(S3_OUTPUT_BUCKET_KEY, new_file_name)
-
-            s3 = boto3.client('s3')
-
+            # ======== Switch to iframe if present ========
             try:
-                s3.upload_file(renamed_file_path, S3_BUCKET_NAME, s3_key)
-                print(f"File uploaded to S3: s3://{S3_BUCKET_NAME}/{s3_key}")
+                iframe = WebDriverWait(driver, 5).until(
+                    EC.presence_of_element_located((By.TAG_NAME, "iframe"))
+                )
+                driver.switch_to.frame(iframe)
+            except Exception:
+                print("No iframe found or switch not needed.")
 
-                # Delete temporary file after download
-                # shutil.rmtree(download_path)
-            except FileNotFoundError:
-                print("The file was not found")
-            except NoCredentialsError:
-                print("Credentials not available")
+            # Get initial list of files in the download directory
+            initial_files = set(os.listdir(download_path))
+
+            # Use download button
+            download_button = WebDriverWait(driver, 20).until(
+                    EC.element_to_be_clickable(
+                        (By.XPATH, "//button[contains(@onclick, \"downloadXLSX('csv_generale_33'\")]")
+                    )
+                )
+
+            driver.execute_script("arguments[0].scrollIntoView();", download_button)
+            download_button.click()
+
+            print("Download button clicked. Waiting for file to appear...")
+
+            # ======== Wait for the file to be downloaded ========
+            # This loop waits until a new file appears in the directory that is not a temporary Chrome file
+            # and its size stops changing, indicating the download is complete.
+            downloaded_file = None
+            timeout = 60  # seconds
+            start_time = time.time()
+
+            while time.time() - start_time < timeout:
+                current_files = set(os.listdir(download_path))
+                new_files = current_files - initial_files
+
+                for f in new_files:
+                    file_path = os.path.join(download_path, f)
+                    # Exclude Chrome's temporary download files and check for actual files
+                    if not f.startswith(".com.google.Chrome.") and not f.endswith(".crdownload"):
+                        # Check if the file size is stable (download complete)
+                        initial_size = -1
+                        for _ in range(5):
+
+                            try:
+                                current_size = os.path.getsize(file_path)
+                            except FileNotFoundError:
+                                continue
+
+                            if initial_size == current_size and current_size > 0:
+                                downloaded_file = f
+                                break
+                            initial_size = current_size
+                            time.sleep(0.5)
+                        if downloaded_file:
+                            break
+                if downloaded_file:
+                    break
+                time.sleep(1)
+
+            driver.quit()
+
+            if downloaded_file:
+                new_file_name = f"poverty_family_structure_{name}.xlsx"
+
+                local_file_path = os.path.join(download_path, downloaded_file)
+                renamed_file_path = os.path.join(download_path, new_file_name)
+                # Rename the downloaded file
+                os.rename(local_file_path, renamed_file_path)
+                s3_key = os.path.join(S3_OUTPUT_BUCKET_KEY, new_file_name)
+
+                s3 = boto3.client('s3')
+
+                try:
+                    s3.upload_file(renamed_file_path, S3_BUCKET_NAME, s3_key)
+                    print(f"File uploaded to S3: s3://{S3_BUCKET_NAME}/{s3_key}")
+
+                    # Delete temporary file after download
+                    # shutil.rmtree(download_path)
+                except FileNotFoundError:
+                    print("The file was not found")
+                except NoCredentialsError:
+                    print("Credentials not available")
+    download_file()
+
+def download_json():
+
+    S3_INPUT_FILE = 'data/municipality_polygon/municipality_polygon_map.geojson'
+    S3_FILE_LOCAL_TMP = "downloads/json-files/municipality_polygon_map.json"
+    s3.download_file(S3_BUCKET_NAME, S3_INPUT_FILE, S3_FILE_LOCAL_TMP)
