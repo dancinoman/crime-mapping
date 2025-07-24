724cf8cef47bf739ede5c5f192e46ee03f22a9aa
Hugo Guay <dancinoman@gmail.com>
2025-07-24 10:27:25 -0400

    fixed the order of operations

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ from app import export as ex
 
 if __name__ == "__main__":
 
-    print("Starting the download process...")
-    df.download_file_from_url()
-    print("File downloaded now converting to CSV...")
-    conv.convert_poverty_files()
-    print("CSV files created, now uploading to S3...")
-    ex.upload_csv_to_s3()
-    print("Uploaded CSV files to S3")
-    print("Now converting JSON files to stackable data...")
+    print("Converting JSON files to stackable data...")
     print("Downloading municipality polygon map...")
     df.download_json()
     conv.convert_geojson()
     ex.upload_json_to_s3()
     print("JSON files converted and uploaded to S3")
+
+    # Start the process of downloading files on web
+    print("Starting the download process...")
+    df.download_file_from_url()
+    print("File downloaded now converting to CSV...")
+    conv.convert_poverty_files()
+    print("Starting to upload to S3...")
+    ex.upload_csv_to_s3()
+    print("Uploaded CSV files to S3")
     print("Pipeline completed successfully!")
