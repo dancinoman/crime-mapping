51b6431b0260c1859b24560a69068585291eb96d
Hugo Guay <dancinoman@gmail.com>
2025-07-28 12:12:46 -0400

    fixed skipped result bug and irrelevant files

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ from app import convert as conv
 from app import export as ex
 
 if __name__ == "__main__":
-
+    
+    # Start the process of downloading files on web
+    print("Starting the download process...")
+    df.download_file_from_url()
     print("Starting the conversion process...")
     conv.convert_poverty_files()
     print("Starting to upload to S3...")
     ex.upload_csv_to_s3()
     print("Uploaded CSV files to S3")
-
-    # Start the process of downloading files on web
-    print("Starting the download process...")
-    df.download_file_from_url()
     print("Pipeline completed successfully!")
