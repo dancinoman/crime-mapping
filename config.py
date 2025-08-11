7e0da17afe3ca9eb08e5632fbc3fc3aef28c7d1c
Hugo Guay <dancinoman@gmail.com>
2025-08-11 16:58:30 -0400

    added a feature of transforming geojson

--- config.py
+++ config.py
@@ -1,15 +1,14 @@
 import os
 
 class Path:
-    def __init__(self, download_path, subfolder_source, subfolder_destination = None):
+    def __init__(self, folder_path, subfolder_source, subfolder_destination = None):
         self.project_root = os.path.dirname(os.path.abspath(__file__))
-        self.download_path = download_path
+        self.folder_path = folder_path
         self.subfolder_source = subfolder_source
         self.subfolder_destination = subfolder_destination
 
     def get_source_path(self):
-        return os.path.join(self.project_root, self.download_path, self.subfolder_source)
+        return os.path.join(self.project_root, self.folder_path, self.subfolder_source)
 
     def get_destination_path(self):
-        return os.path.join(self.project_root, self.download_path, self.subfolder_destination)
-
+        return os.path.join(self.project_root, self.folder_path, self.subfolder_destination)
